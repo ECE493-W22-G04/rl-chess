0e3e20c826a88f75504a669148d3aa674f87129c
Josh Patrick <jjpatric@ualberta.ca>
2022-03-01 18:01:00 -0700

    Add basic test login with jwt

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const token = sessionStorage.getItem('token');
+    console.log('This is your token', token);
+
+    const handleClick = () => {
+        const opts = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+        };
+
+        fetch('http://localhost:5555/token', opts)
+            .then((resp) => {
+                if (resp.status === 200) return resp.json();
+                else alert('There has been some error');
+            })
+            .then((data) => {
+                sessionStorage.setItem('token', data.access_token);
+            })
+            .catch((error) => {
+                console.error('There was an error:' + error);
+            });
+    };
+
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <h1>Login</h1>
+                {token && token != '' && token != undefined ? (
+                    'You are logged in with ' + token
+                ) : (
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button onClick={handleClick}>Login</button>
+                    </div>
+                )}
             </header>
         </div>
     );
--- server/api/routes.py
+++ server/api/routes.py
@@ -1,19 +1,27 @@
+import os
+
 from flask import Flask, request, jsonify, url_for, Blueprint
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
-routes = Blueprint("routes", __name__)
+api = Blueprint("api", __name__)
 
-
-@routes.route("/", methods=["POST", "GET"])
+@api.route("/", methods=["POST", "GET"])
 def handle_default():
     response_body = {"message": "This is the default route for this app, you can write more routes here"}
     return jsonify(response_body), 200
 
 
-@routes.route("/hello", methods=["POST", "GET"])
-def handle_hello():
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
