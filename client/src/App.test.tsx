b3070225d1eb4926002c0538b338785983011f1a
Armianto Sumitro <armiantos@gmail.com>
2022-03-06 15:45:44 -0700

    Provide enzyme test example

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
 test('renders learn react link', () => {
     render(<App />);
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
+});
+
+test('contains logo', () => {
+    const res = shallow(<App />);
+    const imageNode = res.find('img');
+
+    expect(imageNode.prop('alt')).toBe('logo');
 });
