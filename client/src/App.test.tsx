c06e432d58b3d5afa6b8403373742c749a08b012
Armianto Sumitro <armiantos@gmail.com>
2022-03-09 20:44:14 -0700

    Add temporary mocks

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import App from './App';
 import { shallow } from 'enzyme';
 import { Link } from 'react-router-dom';
+import socket from './services/socket';
+import { io } from 'socket.io-client';
+
+jest.mock('./services/socket');
+const mockSocket = jest.mocked(socket);
+
+jest.mock('socket.io-client');
+const mockedIo = jest.mocked(io);
+mockedIo.mockReturnValue(mockSocket);
 
 test('renders RL Chess homepage', () => {
     const resp = shallow(<App />);
@@ -12,6 +21,5 @@ test('renders RL Chess homepage', () => {
 test('contains logo', () => {
     const res = shallow(<App />);
     const imageNode = res.find('img');
-
     expect(imageNode.prop('alt')).toBe('logo');
 });
