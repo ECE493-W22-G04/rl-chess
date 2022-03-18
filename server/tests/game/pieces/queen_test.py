fc77bf38b5d04463286d56f1d91ed4e78dd45b11
Armianto Sumitro <armiantos@gmail.com>
2022-03-17 22:08:14 -0600

    Test diagonal move queen

--- server/tests/game/pieces/queen_test.py
+++ server/tests/game/pieces/queen_test.py
@@ -8,8 +8,30 @@ def test_validate_queen_diagonal():
     pawn = Square(2, 6)
     board.state[pawn.y][pawn.x] = Piece.NONE
 
-    print(board)
+    queen_diagonal = Move(Square(3, 7), Square(0, 4))
+    assert board.validate_move(queen_diagonal)
+
+
+def test_move_queen_diagonal():
+    board = Board()
+
+    pawn = Square(2, 6)
+    board.state[pawn.y][pawn.x] = Piece.NONE
 
     queen_diagonal = Move(Square(3, 7), Square(0, 4))
-    print(board.state[7][3])
-    assert board.validate_move(queen_diagonal)
+
+    back_row = [Piece.ROOK, Piece.KNIGHT, Piece.BISHOP, Piece.QUEEN, Piece.KING, Piece.BISHOP, Piece.KNIGHT, Piece.ROOK]
+    front_row = [Piece.PAWN for _ in range(8)]
+    expected_state = [
+        [piece * -1 for piece in back_row],
+        [piece * -1 for piece in front_row],
+        [Piece.NONE] * 8,
+        [Piece.NONE] * 8,
+        [Piece.QUEEN, *[Piece.NONE] * 7],
+        [Piece.NONE] * 8,
+        [Piece.PAWN, Piece.PAWN, Piece.NONE, *[Piece.PAWN] * 5],
+        [Piece.ROOK, Piece.KNIGHT, Piece.BISHOP, Piece.NONE, Piece.KING, Piece.BISHOP, Piece.KNIGHT, Piece.ROOK],
+    ]
+
+    board.register_move(queen_diagonal)
+    assert board.state == expected_state
