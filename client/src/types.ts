d3b97b4ca81bd4a352b28a325169b0227e6a95b3
Armianto Sumitro <armiantos@gmail.com>
2022-03-19 22:01:02 -0600

    Define gametypes

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,3 +1,18 @@
+export type Board = {
+    state: number[][];
+    isWhiteTurn: boolean;
+};
+
+export type Player = string; // Player is identified by their email (can be found in their JWT)
+
+export type Game = {
+    id: number;
+    board: Board;
+    whitePlayer: Player;
+    blackPlayer: Player;
+    host: Player;
+};
+
 export interface Jwt {
     fresh: boolean;
     iat: number;
