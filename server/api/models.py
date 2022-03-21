442f52505d05f7c4009482dab8a24c0f9997d35d
Armianto Sumitro <armiantos@gmail.com>
2022-03-21 14:25:16 -0600

    Define game model

--- server/api/models.py
+++ server/api/models.py
@@ -1,4 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from uuid import uuid4
+
+from server.api.exceptions import PlayerDoesNotExist
 
 db = SQLAlchemy()
 
@@ -12,3 +15,24 @@ class Player(db.Model):
 
     def __repr__(self):
         return f"<Player {self.email}>"
+
+
+class Game:
+
+    def __init__(self, host_player_id: int) -> None:
+        self.id = uuid4()
+        if Player.query.get(host_player_id) == None:
+            raise PlayerDoesNotExist()
+        self.host = host_player_id
+        self.black_player = None
+        self.white_player = None
+
+    def set_white_player(self, player_id: int):
+        if Player.query.get(player_id) == None:
+            raise PlayerDoesNotExist()
+        self.white_player = player_id
+
+    def set_black_player(self, player_id: int):
+        if Player.query.get(player_id) == None:
+            raise PlayerDoesNotExist()
+        self.black_player = player_id
