fc8c21e1a3d093d9e2ef2fdf1185edb4b158fc57
Armianto Sumitro <armiantos@gmail.com>
2022-03-21 23:28:37 -0600

    Add missing board state

--- client/src/services/game.test.ts
+++ client/src/services/game.test.ts
@@ -17,6 +17,18 @@ const mockGame: Game = {
     whitePlayer: null,
     blackPlayer: null,
     host: 'bob@gmail.com',
+    board: {
+        state: [
+            [-4, -3, -2, -5, -6, -2, -3, -4],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [4, 3, 2, 5, 6, 2, 3, 4],
+        ],
+        isWhiteTurn: true,
+    },
 };
 
 describe('createGame', () => {
