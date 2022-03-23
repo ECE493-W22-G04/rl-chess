66cdc329931441360f05dabeec616c837dea77bc
Armianto Sumitro <armiantos@gmail.com>
2022-03-22 21:56:08 -0600

    Clean up action generator

--- server/game/actions.py
+++ server/game/actions.py
@@ -17,17 +17,11 @@ for from_coordinate in itertools.product(range(8), repeat=2):
         ACTIONS.append(Move(Square(from_coordinate[0], from_coordinate[1]), Square(from_coordinate[0], y)))
     # Diagonal moves
     for increment in range(-7, 8):
-        to_x = from_coordinate[0] + increment
-        to_y = from_coordinate[1] + increment
-        if to_x < 0 or to_x > 7 or to_y < 0 or to_y > 7:
-            continue
-        ACTIONS.append(Move(Square(from_coordinate[0], from_coordinate[1]), Square(to_x, to_y)))
-    for increment in range(-7, 8):
-        to_x = from_coordinate[0] - increment
-        to_y = from_coordinate[1] + increment
-        if to_x < 0 or to_x > 7 or to_y < 0 or to_y > 7:
-            continue
-        ACTIONS.append(Move(Square(from_coordinate[0], from_coordinate[1]), Square(to_x, to_y)))
+        for to_x in [from_coordinate[0] + increment, from_coordinate[0] - increment]:
+            to_y = from_coordinate[1] + increment
+            if to_x < 0 or to_x > 7 or to_y < 0 or to_y > 7:
+                continue
+            ACTIONS.append(Move(Square(from_coordinate[0], from_coordinate[1]), Square(to_x, to_y)))
     # Knight moves
     for x_incr in range(-2, 3):
         for y_incr in range(-2, 3):
