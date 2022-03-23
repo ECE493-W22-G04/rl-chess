59ae0d934a97e68ba0bbdc807a98b95df899bb5b
Robert Mah <rjmah1@ualberta.ca>
2022-03-22 22:32:57 -0600

    not working main menu test

---
+++ client/src/components/MainMenu.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from '../App';
+import MainMenu from './MainMenu';
+import { shallow } from 'enzyme';
+
+const handlePlayAgainstComputer = jest.fn();
+
+test('pvc calls correct handler', () => {
+    // const res = shallow(<App />);
+    // const mainMenu = res.find(<MainMenu />);
+    // const pvcButton = mainMenu.find('#pvc');
+    // pvcButton.simulate('click');
+    // expect(handlePlayAgainstComputer).toHaveBeenCalled();
+    expect(true);
+});
