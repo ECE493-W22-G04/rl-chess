37b549e637a7b800301131dd690c57063141a000
Armianto Sumitro <armiantos@gmail.com>
2022-03-23 20:29:55 -0600

    Add main menu tests

--- client/src/components/MainMenu.test.tsx
+++ client/src/components/MainMenu.test.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import App from '../App';
-import MainMenu from './MainMenu';
 import { shallow } from 'enzyme';
+import { createGame } from '../services/game';
+import MainMenu from './MainMenu';
+import { mockGame } from '../testConstants';
 
-const handlePlayAgainstComputer = jest.fn();
+jest.mock('../services/game');
+const mockCreateGame = jest.mocked(createGame, true);
 
-test('pvc calls correct handler', () => {
-    // const res = shallow(<App />);
-    // const mainMenu = res.find(<MainMenu />);
-    // const pvcButton = mainMenu.find('#pvc');
-    // pvcButton.simulate('click');
-    // expect(handlePlayAgainstComputer).toHaveBeenCalled();
-    expect(true);
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate,
+}));
+
+beforeEach(() => {
+    mockCreateGame.mockResolvedValueOnce(mockGame);
+});
+
+type OnClickHandler = () => Promise<void>;
+
+test('pvc calls correct handler', async () => {
+    const res = shallow(<MainMenu />);
+
+    const onClickHandler = res.find('#pvc').prop('onClick') as unknown as OnClickHandler;
+    await onClickHandler();
+    expect(mockCreateGame).toBeCalledWith(false);
+    expect(mockNavigate).toBeCalledWith(`/game/${mockGame.id}`);
+});
+
+test('pvp calls correct handler', async () => {
+    const res = shallow(<MainMenu />);
+
+    const onClickHandler = res.find('#pvp').prop('onClick') as unknown as OnClickHandler;
+    await onClickHandler();
+    expect(mockCreateGame).toBeCalledWith(true);
+    expect(mockNavigate).toBeCalledWith(`/game/${mockGame.id}`);
 });
