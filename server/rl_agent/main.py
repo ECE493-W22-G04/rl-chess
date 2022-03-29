c14cafd1cea1c2dd6c1d39a5488b9f3b6a2e0ca0
Armianto Sumitro <armiantos@gmail.com>
2022-03-29 02:03:43 -0600

    Serialize model

--- server/rl_agent/main.py
+++ server/rl_agent/main.py
@@ -1,5 +1,6 @@
-import numpy as np
+from os.path import exists
 
+import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Flatten
 from tensorflow.keras.optimizers import Adam
@@ -36,13 +37,17 @@ policy = BoltzmannQPolicy()
 dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10, target_model_update=1e-2, policy=policy)
 dqn.compile(Adam(learning_rate=1e-3), metrics=['mae'])
 
+WEIGHTS_FILE = 'dqn_chess_weights.h5f'
+if exists(WEIGHTS_FILE):
+    dqn.load_weights(WEIGHTS_FILE)
+
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-dqn.fit(env, nb_steps=50000, visualize=True, verbose=2)
+dqn.fit(env, nb_steps=50000, visualize=False, verbose=1)
 
 # After training is done, we save the final weights.
-dqn.save_weights(f'dqn_chess_weights.h5f', overwrite=True)
+dqn.save_weights(WEIGHTS_FILE, overwrite=True)
 
 # Finally, evaluate our algorithm for 5 episodes.
-dqn.test(env, nb_episodes=5, visualize=True)
+dqn.test(env, nb_episodes=5, visualize=False)
