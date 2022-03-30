c12cbeca1b111904e995ed50850d23edb3082148
Armianto Sumitro <armiantos@gmail.com>
2022-03-30 02:15:58 -0600

    Add rl_agent test

---
+++ server/tests/agent_tests.py
@@ -0,0 +1,9 @@
+from server.rl_agent.agent import RlAgent
+from server.game.board import Board
+
+def test_rl_can_predict_moves():
+    rl_agent = RlAgent()
+    board = Board()
+    for _ in range(10):
+        action = rl_agent.predict(board)
+        assert board.register_move(action)
