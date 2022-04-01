52175ecf0b1fc2c22730101f2f0fc96f359ee9c9
Armianto Sumitro <armiantos@gmail.com>
2022-03-31 20:08:40 -0600

    Fix import errors

--- server/tests/agent_test.py
+++ server/tests/agent_test.py
@@ -1,10 +1,16 @@
-from server.rl_agent.agent import RlAgent
-from server.game.board import Board
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from ..rl_agent.agent import RlAgent
+from ..game.board import Board
+
 
 
 def test_rl_can_predict_moves():
     rl_agent = RlAgent()
     board = Board()
-    for i in range(5):
+    for i in range(3):
         action = rl_agent.predict(board)
         assert board.register_move(action)
