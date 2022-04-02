6975d3f72cb2f523e087c4ac8b302f9c4b00a1ba
armiantos <43416725+armiantos@users.noreply.github.com>
2022-04-01 18:35:24 -0600

    Define RL model for chess (#31)
    * Create chess env for open ai gym
    
    * Use dqn to train with chess env
    
    * Add RL dependencies
    
    * Serialize model
    
    * Remove unwanted packages
    
    * Revert unwanted changes
    
    * Fix save model implementation and increase learning rate
    
    * Add index variants for action fetchers
    
    * Jump start the agent to learn valid moves first
    
    * Format files
    
    * Turn agent into class
    
    * Set number of episodes to train as hyperparameter
    
    * Fix import errors
    
    * Rename main to train
    
    * Remove unused imports
    
    * Print warning when agent makes illegal move
    
    * Add rl_agent test
    
    * Fix format issues
    
    * Revert pytest bump
    
    * Fix logging error
    
    * Fix programming error
    
    * Fix agent tests not running

---
+++ server/rl_agent/agent.py
@@ -0,0 +1,65 @@
+from sys import stderr
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Flatten
+from tensorflow.keras.optimizers import Adam
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+from server.rl_agent.chess_env import ChessEnv
+from server.game.board import Board
+from server.game.move import Move
+
+
+class RlAgent():
+    WEIGHTS_FILE = 'checkpoints/checkpoint'
+
+    def __init__(self) -> None:
+        self.__agent = self.__build_agent()
+        self.__load_weights()
+
+    def __build_agent(self) -> DQNAgent:
+        env = ChessEnv()
+        nb_actions = env.action_space.n
+
+        model = Sequential()
+        model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+        model.add(Dense(16))
+        model.add(Activation('relu'))
+        model.add(Dense(16))
+        model.add(Activation('relu'))
+        model.add(Dense(16))
+        model.add(Activation('relu'))
+        model.add(Dense(nb_actions))
+        model.add(Activation('linear'))
+
+        memory = SequentialMemory(limit=50000, window_length=1)
+        policy = BoltzmannQPolicy()
+        dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=50, target_model_update=1e-2, policy=policy)
+        dqn.compile(Adam(learning_rate=1e-1), metrics=['mae'])
+        return dqn
+
+    def __load_weights(self):
+        try:
+            self.__agent.load_weights(self.WEIGHTS_FILE)
+        except Exception as err:
+            print('Could not find weights file, initializing new one', file=stderr)
+            self.train(100)
+
+    def predict(self, board: Board) -> Move:
+        actions = board.get_actions()
+        predicted_move_index = self.__agent.forward(board.state)
+        predicted_move = actions[predicted_move_index]
+
+        if not predicted_move in board.get_legal_actions():
+            print(f'Agent provided illegal move {predicted_move}', file=stderr)
+            random_action_index = np.random.choice(board.get_legal_action_indices())
+            return actions[random_action_index]
+
+        return predicted_move
+
+    def train(self, num_episodes: int = 50000):
+        env = ChessEnv()
+        self.__agent.fit(env, nb_steps=num_episodes, visualize=False, verbose=1)
+        self.__agent.save_weights(self.WEIGHTS_FILE, overwrite=True)
