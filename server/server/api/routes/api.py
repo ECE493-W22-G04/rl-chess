1be73fbae66ad91340f095356bc115485168f5e5
Armianto Sumitro <armiantos@gmail.com>
2022-04-03 17:48:38 -0600

    Revert leaderboard changes

--- server/server/api/routes/api.py
+++ server/server/api/routes/api.py
@@ -2,12 +2,10 @@ from flask import Blueprint, jsonify, request
 
 from .auth import auth
 from .games import game
-from .leaderboard import leaderboard
 
 api = Blueprint("api", __name__, url_prefix="/api")
 api.register_blueprint(auth)
 api.register_blueprint(game)
-api.register_blueprint(leaderboard)
 
 
 # Create a route to display a homepage message to unauthenticated user
