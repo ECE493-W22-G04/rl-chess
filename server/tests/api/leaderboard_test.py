bc0d77daf7ae05d2542ba6d9e00cf0daf6d21389
Armianto Sumitro <armiantos@gmail.com>
2022-04-04 13:41:16 -0600

    Add ordering test

--- server/tests/api/leaderboard_test.py
+++ server/tests/api/leaderboard_test.py
@@ -33,20 +33,50 @@ def test_leaderboard(client: FlaskClient, app: Flask):
         resp = client.get('/api/leaderboard/', headers={'Authorization': f'Bearer {player1_access_token}'})
         json_response = resp.json
 
-        expected_payloads = [{
+        expected_payload = [{
             'email': player1.email,
             'numWins': 2,
         }]
         assert resp.status_code == 200
-        for expected_payload in expected_payloads:
-            assert expected_payload in json_response
+        assert expected_payload == json_response
 
 
 def test_empty_leaderboard(client: FlaskClient, app: Flask, access_token: str):
     resp = client.get('/api/leaderboard/', headers={'Authorization': f'Bearer {access_token}'})
     json_response = resp.json
 
-    expected_payloads = []
+    expected_payload = []
     assert resp.status_code == 200
-    for expected_payload in expected_payloads:
-        assert expected_payload in json_response
+    assert expected_payload == json_response
+
+
+def test_leaderboard_ordering(client: FlaskClient, app: Flask):
+    with app.app_context():
+        player1 = Player(email=TEST_EMAIL, password=TEST_PASSWORD)
+        player2 = Player(email='asdf@mail.com', password=TEST_PASSWORD)
+        for player in [player1, player2]:
+            db.session.add(player)
+            db.session.commit()
+
+        player1_access_token = create_access_token(identity=player.email)
+
+        pvc_game = Game(host_email=player.email, is_pvp=False)
+        saved_game1 = SavedGame(black_player=player1.id, white_player=None, winner=player1.id, game_history=json.dumps(pvc_game.board.moves), is_pvp=pvc_game.is_pvp)
+        saved_game2 = SavedGame(black_player=player1.id, white_player=None, winner=player1.id, game_history=json.dumps(pvc_game.board.moves), is_pvp=pvc_game.is_pvp)
+        saved_game3 = SavedGame(black_player=player2.id, white_player=None, winner=player2.id, game_history=json.dumps(pvc_game.board.moves), is_pvp=pvc_game.is_pvp)
+        for saved_game in [saved_game1, saved_game2, saved_game3]:
+            db.session.add(saved_game)
+            db.session.commit()
+
+        resp = client.get('/api/leaderboard/', headers={'Authorization': f'Bearer {player1_access_token}'})
+        json_response = resp.json
+
+        expected_payload = [{
+            'email': player1.email,
+            'numWins': 2,
+        }, {
+            'email': player2.email,
+            'numWins': 1,
+        }]
+        assert resp.status_code == 200
+        assert json_response == expected_payload
