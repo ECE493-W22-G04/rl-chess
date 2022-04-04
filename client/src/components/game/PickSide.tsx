d56a89512c5c2c44a5ad71692f4c0ad7093dcc8e
Josh Patrick <jjpatric@ualberta.ca>
2022-04-03 18:25:02 -0600

    Fix White button, add random button

--- client/src/components/game/PickSide.tsx
+++ client/src/components/game/PickSide.tsx
@@ -14,14 +14,22 @@ const PickSide: FC<PickSideProps> = ({ gameId }: PickSideProps) => {
     const handlePickWhite = () => {
         socket.emit('pick_side', { gameId: gameId, color: 'white', user: AuthService.getCurrentUser() });
     };
+    const handlePickRandom = () => {
+        const random_boolean = Math.random() < 0.5;
+        const color = random_boolean ? 'white' : 'black';
+        socket.emit('pick_side', { gameId: gameId, color: color, user: AuthService.getCurrentUser() });
+    };
     return (
         <div style={{ display: 'flex', flexDirection: 'column', gap: '1em', alignItems: 'center' }}>
             <h3>Select Your Color </h3>
             <Button variant="dark" onClick={handlePickBlack}>
                 Black
             </Button>
-            <Button variant="light" onClick={handlePickWhite}>
+            <Button variant="outline-dark" onClick={handlePickWhite}>
                 White
+            </Button>
+            <Button variant="warning" onClick={handlePickRandom}>
+                Random
             </Button>
         </div>
     );
