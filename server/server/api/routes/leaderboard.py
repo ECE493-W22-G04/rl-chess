0829a31fd0af244089cb37f0b6668b3822323be1
Armianto Sumitro <armiantos@gmail.com>
2022-04-04 13:25:16 -0600

    Implement leaderboard endpoint

---
+++ server/server/api/routes/leaderboard.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, Flask, jsonify, request
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from sqlalchemy import func
+
+from ..models import Game, Player, SavedGame, db
+
+leaderboard = Blueprint("leaderboard", __name__, url_prefix="/leaderboard")
+
+
+@leaderboard.route("/", methods=["GET"])
+def create_game():
+    res = db.session.query(SavedGame.winner, func.count(SavedGame.winner)) \
+        .group_by(SavedGame.winner) \
+        .join(Player, SavedGame.winner == Player.id) \
+        .add_columns(Player.email) \
+        .group_by(Player.email) \
+        .all()
+    payload = [{
+        'email': email,
+        'numWins': num_wins,
+    } for [_id, num_wins, email] in res]
+    return jsonify(payload), 200
---
+++ server/tests/api/leaderboard_test.py
@@ -0,0 +1,47 @@
+import json
+from flask import Flask
+import pytest
+from flask.testing import FlaskClient
+from flask_jwt_extended import create_access_token
+
+from server.api.models import Game, SavedGame, Player, db
+from ..constants import TEST_EMAIL, TEST_PASSWORD
+
+def test_leaderboard(client: FlaskClient, app: Flask):
+    with app.app_context():
+        player1 = Player(email=TEST_EMAIL, password=TEST_PASSWORD)
+        player2 = Player(email='asdf@mail.com', password=TEST_PASSWORD)
+        for player in [player1, player2]:
+            db.session.add(player)
+            db.session.commit()
+
+        pvc_game = Game(host_email=player.email, is_pvp=False)
+        saved_game1 = SavedGame(black_player=player1.id, white_player=None, winner=player1.id, game_history=json.dumps(pvc_game.board.moves), is_pvp=pvc_game.is_pvp)
+        saved_game2 = SavedGame(black_player=player1.id, white_player=None, winner=player1.id, game_history=json.dumps(pvc_game.board.moves), is_pvp=pvc_game.is_pvp)
+        saved_game3 = SavedGame(black_player=player2.id, white_player=None, winner=None, game_history=json.dumps(pvc_game.board.moves), is_pvp=pvc_game.is_pvp)
+        for saved_game in [saved_game1, saved_game2, saved_game3]:
+            db.session.add(saved_game)
+            db.session.commit()
+
+        resp = client.get('/api/leaderboard/')
+        json_response = resp.json
+
+        expected_payloads = [
+            {
+                'email': player1.email,
+                'numWins': 2,
+            }
+        ]
+        assert resp.status_code == 200
+        for expected_payload in expected_payloads:
+            assert expected_payload in json_response
+
+def test_empty_leaderboard(client: FlaskClient, app: Flask):
+    with app.app_context():
+        resp = client.get('/api/leaderboard/')
+        json_response = resp.json
+
+        expected_payloads = []
+        assert resp.status_code == 200
+        for expected_payload in expected_payloads:
+            assert expected_payload in json_response
