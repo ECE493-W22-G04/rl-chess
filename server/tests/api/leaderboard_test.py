6763873309e6d9236f722c3708f840243bcafff0
Armianto Sumitro <armiantos@gmail.com>
2022-04-04 13:39:14 -0600

    Add unauthed request test

--- server/tests/api/leaderboard_test.py
+++ server/tests/api/leaderboard_test.py
@@ -1,11 +1,15 @@
 import json
 from flask import Flask
-import pytest
 from flask.testing import FlaskClient
 from flask_jwt_extended import create_access_token
 
 from server.api.models import Game, SavedGame, Player, db
 from ..constants import TEST_EMAIL, TEST_PASSWORD
+
+
+def test_leaderboard_unauthed(client: FlaskClient):
+    resp = client.get('/api/leaderboard/')
+    assert resp.status_code == 401
 
 
 def test_leaderboard(client: FlaskClient, app: Flask):
@@ -16,6 +20,8 @@ def test_leaderboard(client: FlaskClient, app: Flask):
             db.session.add(player)
             db.session.commit()
 
+        player1_access_token = create_access_token(identity=player.email)
+
         pvc_game = Game(host_email=player.email, is_pvp=False)
         saved_game1 = SavedGame(black_player=player1.id, white_player=None, winner=player1.id, game_history=json.dumps(pvc_game.board.moves), is_pvp=pvc_game.is_pvp)
         saved_game2 = SavedGame(black_player=player1.id, white_player=None, winner=player1.id, game_history=json.dumps(pvc_game.board.moves), is_pvp=pvc_game.is_pvp)
@@ -24,7 +30,7 @@ def test_leaderboard(client: FlaskClient, app: Flask):
             db.session.add(saved_game)
             db.session.commit()
 
-        resp = client.get('/api/leaderboard/')
+        resp = client.get('/api/leaderboard/', headers={'Authorization': f'Bearer {player1_access_token}'})
         json_response = resp.json
 
         expected_payloads = [{
@@ -36,12 +42,11 @@ def test_leaderboard(client: FlaskClient, app: Flask):
             assert expected_payload in json_response
 
 
-def test_empty_leaderboard(client: FlaskClient, app: Flask):
-    with app.app_context():
-        resp = client.get('/api/leaderboard/')
-        json_response = resp.json
+def test_empty_leaderboard(client: FlaskClient, app: Flask, access_token: str):
+    resp = client.get('/api/leaderboard/', headers={'Authorization': f'Bearer {access_token}'})
+    json_response = resp.json
 
-        expected_payloads = []
-        assert resp.status_code == 200
-        for expected_payload in expected_payloads:
-            assert expected_payload in json_response
+    expected_payloads = []
+    assert resp.status_code == 200
+    for expected_payload in expected_payloads:
+        assert expected_payload in json_response
