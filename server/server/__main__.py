cdf3250938f9ee09f7e896d58e3a2d9c7a28ba70
Armianto Sumitro <armiantos@gmail.com>
2022-04-05 00:47:33 -0600

    Launch without gunicorn

--- server/server/__main__.py
+++ server/server/__main__.py
@@ -1,12 +1,15 @@
+import os
 from pbu import Logger
 from server.api.config import get_log_folder
 from .__init__ import socketio, app
 
 if __name__ == "__main__":
+    port = os.environ['PORT'] or "5555"
+
     logger = Logger("MAIN", log_folder=get_log_folder())
     logger.info("==========================================")
     logger.info("           Starting application")
     logger.info("==========================================")
 
     # start flask app
-    socketio.run(app, host="0.0.0.0", port="5555")
+    socketio.run(app, host="0.0.0.0", port=port)
