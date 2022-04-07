e71156ef7e9be5322afa513d11b14e417335399b
Robert Mah <43456924+rjmah1@users.noreply.github.com>
2022-04-07 17:24:10 -0600

    Robert/better rl (#77)
    * rl that uses boltsman for prediction, also belief revision happens multiple times per game
    
    * remove unused import
    
    * trained more and reduce observation space

--- server/rl_agent/train.py
+++ server/rl_agent/train.py
@@ -2,4 +2,5 @@ from rl_agent.agent import RlAgent
 
 if __name__ == '__main__':
     rl_agent = RlAgent()
-    rl_agent.train(num_episodes=100)
+    for _ in range(100):
+        rl_agent.train(num_episodes=1000)
